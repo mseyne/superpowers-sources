ffb272202379fdb7cab46ca1e8cc61f49660e725
mseyne <michaelseyne@gmail.com>
2016-02-13 13:16:56 +0100

    remove azerty

--- 1SuperPong/scripts/paddles.ts
+++ 1SuperPong/scripts/paddles.ts
@@ -2,15 +2,15 @@ class Paddle1Behavior extends Sup.Behavior {
   // connect the paddle body to a variable which will be used many times in the script
   pad = this.actor.arcadeBody2D;
 
-  // set the speed of the paddle 
+  // set the speed of the paddle
   speed : number = 0.1;
-    
+
   update() {
     // get Y position of paddle in a variable
     let y : number = this.actor.getY();
-    
+
     // if the key is pressed and y < to max, the velocity of the body is set in motion with speed
-    if(Sup.Input.isKeyDown("Z") && y < 2.35 || Sup.Input.isKeyDown("W") && y < 2.35){
+    if(Sup.Input.isKeyDown("W") && y < 2.35){
       this.pad.setVelocityY(this.speed);
     }
     // if the key is pressedand y > to min, the velocity of the body is set in motion with negative speed
@@ -20,7 +20,7 @@ class Paddle1Behavior extends Sup.Behavior {
     // in other situations the velocity of the body is set to 0
     else{
       this.pad.setVelocityY(0);
-    }  
+    }
   }
 }
 
@@ -28,13 +28,13 @@ class Paddle2Behavior extends Sup.Behavior {
   // connect the paddle body to a variable which will be used many times in the script
   pad = this.actor.arcadeBody2D;
 
-  // set the speed of the paddle 
+  // set the speed of the paddle
   speed : number = 0.1;
-    
+
   update() {
     // get Y position of paddle in a variable
     let y : number = this.actor.getY();
-    
+
     // if the key is pressed and y < to max, the velocity of the body is set in motion with speed
     if(Sup.Input.isKeyDown("UP") && y < 2.35){
       this.pad.setVelocityY(this.speed);
