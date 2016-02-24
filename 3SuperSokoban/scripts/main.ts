a9c4897585b11af98e59970e627bd57288d6d96c
mseyne <michaelseyne@gmail.com>
2016-02-24 11:47:28 +0100

    fix

--- 3SuperSokoban/scripts/main.ts
+++ 3SuperSokoban/scripts/main.ts
@@ -47,7 +47,7 @@ namespace Game{
       levelMax++;
     }
   }
-  
+
   export function getPosition(level){
     /*
       Scan the 16x12 level in order to :
@@ -55,39 +55,39 @@ namespace Game{
       - Set the playerPosition vector from the Start tile position on Actor layer
       - Change the Start tile by an empty tile (the Player Sprite will come instead)
     */
-    
+
     // Set the variables to default, erase previous level
     mapSaved = [];
     playerPosition.x = 0, playerPosition.y = 0;
-    
+
     for(let row = 0; row < 12; row++){
       for(let column = 0; column < 16; column++){
-        
+
         // get the tile for x = column and y = row positions
         let actorTile = level.getTileAt(Layers.Actors, column, row);
-        
+
         // Add the tile to the array
         mapSaved.push(actorTile);
-          
+
         if(actorTile === Tiles.Start){
           // remove the Start tile and replace with empty tile
           level.setTileAt(Layers.Actors, column, row, Tiles.Empty);
-          
+
           // set position to x, y on level map
           playerPosition.add(column, row);
         }
       }
     }
   }
-  
+
   export function checkLevel(level){
 
     /*
     We check all the level for the crates and targets positions.
-    We then compare if the position of crates and targets match, if all crate are on target, the level is won.
+    We then compare if the position of crates and targets match,
+    if all crate are on target, the level is won.
     */
 
-    // add all vector of crates and end position in the game and compare them together, if the all equal, the game is finished
     let boxesNumber : number = 0;
     let boxesPositions = [];
     let targetsPositions = [];
@@ -108,7 +108,7 @@ namespace Game{
           boxesNumber++;
         }
 
-        // If the world tile is a target, keep the position 
+        // If the world tile is a target, keep the position
         if(worldTile === Tiles.Target){
           let position = new Sup.Math.Vector2(column, row);
           targetsPositions.push(position);
@@ -124,14 +124,14 @@ namespace Game{
   }
 
   function checkVictory(level, boxesNumber, boxesPositions, targetsPositions){
-    
+
     /*
     Check all the positions and find if the coordinate match together.
     If there is as much match than there is boxes, the game is finished.
     */
-    
+
     let count : number = 0;
-    
+
     for(let posBox of boxesPositions){
       for(let posTarget of targetsPositions){
         if(posBox.x === posTarget.x && posBox.y === posTarget.y){
@@ -143,17 +143,17 @@ namespace Game{
       return true;
     }
   }
-  
+
   export function setLevel(){
         //reset values to default
         isLevelWon = false;
         //reload the scene
         Sup.loadScene("Game");
   }
-  
+
   export function resetLevel(level){
     let index : number = 0;
-    
+
     // set all the actor tiles of the current level to the savedMap tile
     for(let row = 0; row < 12; row++){
       for(let column = 0; column < 16; column++){
@@ -163,7 +163,7 @@ namespace Game{
     }
     // call the setLevel function to prepare a new level
     setLevel();
-  } 
+  }
 }
 
 // Call the getMaxLevel function when game is launched
